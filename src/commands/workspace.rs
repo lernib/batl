ce70e86de5cbab23fa644ba5ea64449c9d6f6efa
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-02 07:25:08 -0600

    Added workspace init/delete

--- src/commands/workspace.rs
+++ src/commands/workspace.rs
@@ -1,17 +1,31 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 use clap::Subcommand;
-use crate::utils::{get_batl_root, UtilityError};
+use crate::utils::{get_batl_root, write_toml, UtilityError, BATL_NAME_REGEX};
+use crate::config::*;
 
 #[derive(Subcommand)]
 pub enum Commands {
-  Ls
+  Ls,
+  Init {
+    name: String
+  },
+  Delete {
+    name: String
+  }
 }
 
 pub fn run(cmd: Commands) -> Result<(), UtilityError> {
   match cmd {
     Commands::Ls => {
       cmd_ls()
+    },
+    Commands::Init { name } => {
+      cmd_init(name)
+    },
+    Commands::Delete { name } => {
+      cmd_delete(name)
     }
   }
 }
@@ -51,6 +65,69 @@ fn cmd_ls() -> Result<(), UtilityError> {
   for name in found {
     println!("{}", name);
   }
+
+  Ok(())
+}
+
+fn cmd_init(name: String) -> Result<(), UtilityError> {
+  if !BATL_NAME_REGEX.is_match(&name) {
+    return Err(UtilityError::InvalidName(name));
+  }
+
+  let workspace_root = get_batl_root()?.join("workspaces");
+
+  let parts = name.split('/').collect::<Vec<&str>>();
+  let mut path = workspace_root;
+
+  for part in parts.iter().take(parts.len() - 1) {
+    path = path.join(format!("@{}", part));
+  }
+  path = path.join(parts.last().unwrap());
+
+  if path.exists() {
+    return Err(UtilityError::ResourceAlreadyExists(format!("Workspace {}", name)));
+  }
+
+  std::fs::create_dir_all(path.clone())?;
+
+  let batl_toml_path = path.join("batl.toml");
+  let config = Config {
+    environment: EnvConfig {
+      version: env!("CARGO_PKG_VERSION").to_string(),
+    },
+    workspace: Some(HashMap::new()),
+    repository: None
+  };
+
+  write_toml(&batl_toml_path, &config)?;
+
+  println!("Workspace {} created", name);
+
+  Ok(())
+}
+
+fn cmd_delete(name: String) -> Result<(), UtilityError> {
+  if !BATL_NAME_REGEX.is_match(&name) {
+    return Err(UtilityError::InvalidName(name));
+  }
+
+  let workspace_root = get_batl_root()?.join("workspaces");
+
+  let parts = name.split('/').collect::<Vec<&str>>();
+  let mut path = workspace_root;
+
+  for part in parts.iter().take(parts.len() - 1) {
+    path = path.join(format!("@{}", part));
+  }
+  path = path.join(parts.last().unwrap());
+
+  if !path.exists() {
+    return Err(UtilityError::ResourceDoesNotExist(format!("Workspace {}", name)));
+  }
+
+  std::fs::remove_dir_all(path)?;
+
+  println!("Workspace {} deleted", name);
 
   Ok(())
 }
